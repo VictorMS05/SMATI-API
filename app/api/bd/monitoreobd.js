32d5b5917627cae478781d7bda64bb78ad78b61a
VictorMS05 <135737262+VictorMS05@users.noreply.github.com>
2024-02-25 10:36:37 -0600

    agregue variable de entorno DB_URL y deshabilite las demas

--- app/api/bd/monitoreobd.js
+++ app/api/bd/monitoreobd.js
@@ -1,22 +1,23 @@
 require('dotenv').config();
 
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const { promisify } = require('util');
 // Configuración de la base de datos
 const fs = require('fs');
 const rawConfig = fs.readFileSync('api/bd/appconfig.json');
 const config = JSON.parse(rawConfig);
 // Reemplazar los marcadores de posición con valores de variables de entorno
-config.conexionDB.host = process.env.DB_HOST;
-config.conexionDB.port = process.env.DB_PORT;
-config.conexionDB.user = process.env.DB_USER;
-config.conexionDB.password = process.env.DB_PASSWORD;
-config.conexionDB.database = process.env.DB_NAME;
+// config.conexionDB.host = process.env.DB_HOST;
+// config.conexionDB.port = process.env.DB_PORT;
+// config.conexionDB.user = process.env.DB_USER;
+// config.conexionDB.password = process.env.DB_PASSWORD;
+// config.conexionDB.database = process.env.DB_NAME;
+// config.conexionDB.url = process.env.DB_URL;
 
-const conexionBD = config.conexionDB;
+// const conexionBD = config.conexionDB;
 // const conexionBD = require('./appconfig.json').conexionDB;
 
-const pool = mysql.createPool(conexionBD);
+const pool = mysql.createPool(process.env.DB_URL);
 pool.getConnection((err, con) => {
     if (err) {
         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
